69e3d2417f04e08a1cdfa152d9b020493a54400d
amalia barrigas <amaliabarrigas@gmail.com>
2025-04-23 15:30:33 +0200

    actividad realizada por Amalia B

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +22,11 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let camelTail = part1.charAt(0).toUpperCase() + part1.slice(1) + part2.charAt(0).toUpperCase() + part2.slice(1);
 
 
 // Print the cameLtaiL-formatted string
-
+console.log(camelTail);
 
 
 
@@ -35,10 +36,13 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tipPercentage = 0.15;
+const tipAmount = billTotal * tipPercentage;
+
 
 
 // Print out the tipAmount
-
+console.log(`The tip amount is: $${tipAmount.toFixed(2)}`);
 
 
 
@@ -47,9 +51,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const min = 1;
+const max = 10; 
 
 // Print the generated random number
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(`The random number is: ${randomNumber}`);
 
 
 
@@ -61,16 +68,23 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
+// false because both a and b are not true
 
-const expression2 = a || b;
+const expression2 = a || b; // true
+// true because at least one of a or b is true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false
+// false because a is true and b is false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b);  // true
+// true because a and b are not both true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
+// true because at least one of a or b is false
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false 
+// false because at least one of a or b is true
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+// true because both a and a are true
